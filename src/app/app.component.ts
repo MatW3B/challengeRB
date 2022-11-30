84a83350e2b350d513ccd2beafe4c67f4647a36f
Hawwu <mateusz323maj@gmail.com>
2022-11-30 17:55:16 +0100

    API data fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  readonly API_URL = 'https://620e9760ec8b2ee28326ae84.mockapi.io/api/1/users';
   title = 'challenge-rb';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(this.API_URL).subscribe({
+      next: (v) => {
+        // do sth with value
+      }
+    });
+  }
+
 }
